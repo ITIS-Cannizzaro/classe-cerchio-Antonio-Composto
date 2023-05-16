c369b7670a3fb5e9407d897503faf9dcf146feda
MattiaDurante <119576786+MattiaDurante@users.noreply.github.com>
2023-05-16 12:06:56 +0200

    consegna cerchio

--- src/Cognome.java
+++ src/Cognome.java
@@ -1,43 +1,47 @@
 import java.util.Scanner;
 
-// Chiamare la classe col proprio cognome
-public class MainClass
-{
-	static Scanner in = new Scanner(System.in);
-	
-	public static void main(String[] args)
-	{
-		while(true)
-		{
-			stampaMenu();
-			int scelta = Integer.parseInt(in.nextLine());
-			switch(scelta)
-			{
-				//aggiungere, togliere casi a seconda delle proprie scelte
-				case 1:
-					//Inserire metodo statico
-				break;
-				case 2:
-					//Inserire metodo statico
-				break;
-				case 3:
-					//Inserire metodo statico
-				break;
-				default:
-					System.out.println("Scelta errata, riprova!");
+public class Cognome {
+    
+    public static void main(String[] args){
+        
 
-			}
-		}
-	}
-	static void stampaMenu()
-	{
-		// Modificare il menù secondo le proprie scelte
-		System.out.println("1 - Es n. * - Titolo es. *");
-		System.out.println("2 - Es n. * - Titolo es. *");
-		System.out.println("3 - Es n. * - Titolo es. *");
-		System.out.println("4 - Es n. * - Titolo es. *");
-		System.out.println("5 - Es n. * - Titolo es. *");
-	}
-	
-	// Aggiungere qui i metodi statici relativi agli esercizi scelti
+        Scanner sca=new Scanner(System.in);
+
+        System.out.println("inserire raggio 1 " );
+        double r=sca.nextDouble();
+        
+        cerchio dati=new cerchio(1,1, 1, 1);
+        dati.calcraggio(r);
+        dati.calcDiametro(r);
+        dati.calArea(r);
+        dati.calperimetro(r);
+        System.out.println("caratteristiche cerchio 1" );
+        dati.stampa();
+        System.out.println("" );
+        System.out.println("inserire raggio 2 " );
+        r=sca.nextDouble();
+        
+        cerchio dati2=new cerchio(1,1, 1, 1);
+        dati2.calcDiametro(r);
+        dati2.calcraggio(r);
+        dati2.calArea(r);
+        dati2.calperimetro(r);
+        System.out.println("caratteristiche cerchio 2" );
+        dati2.stampa();
+
+
+        System.out.println("" ); System.out.println("" );
+        cerchio dati3=new cerchio(1,1, 1, 1);
+        dati3.calcDiametro(dati.raggio+dati2.raggio);
+        dati3.calcraggio(dati.raggio+dati2.raggio);
+        dati3.calArea(dati.raggio+dati2.raggio);
+        dati3.calperimetro(dati.raggio+dati2.raggio);
+        System.out.println("caratteristiche cerchio 3" );
+        dati3.stampa();
+
+
+        
+    }
+
+
 }
---
+++ src/cerchio.java
@@ -0,0 +1,44 @@
+public class cerchio {
+
+    double raggio;
+    double diametro;
+    double perimetro;
+    double area;
+
+    public cerchio(double raggio,double diametro, double perimetro, double area){
+        this.raggio = raggio;
+        this.diametro=diametro;
+        this.perimetro=perimetro;
+        this.area=area;
+
+    }
+
+    public void calcraggio(double r){
+        raggio = r;
+        
+    }
+
+    public void calcDiametro(double r){
+        diametro=r*2;
+        
+    }
+    public void  calperimetro(double r){
+        perimetro=(2*r)*3.14;
+    }
+    public void  calArea(double r){
+        area=(r*r)*3.14;
+    }
+    
+    public void stampa(){
+        System.out.println("il raggio Ã¨ " + raggio );
+        System.out.println("il diametro Ã¨ " + diametro );
+        System.out.println("il perimetro Ã¨ " + perimetro );
+        System.out.println("l'area Ã¨ " + area );
+       
+        
+
+    }
+
+
+
+}
